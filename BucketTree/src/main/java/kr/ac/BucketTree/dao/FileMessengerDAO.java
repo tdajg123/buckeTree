b472ea269957220674121652fb5651eb7cc06142
DESKTOP-09SS2IG\2Seung <tksakseodl@naver.com>
2016-09-22 20:13:03 +0900

    fixing .gitignore

--- BucketTree/src/main/java/kr/ac/BucketTree/dao/FileMessengerDAO.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.BucketTree.dao;
-
-import kr.ac.BucketTree.vo.FileMessengerVO;
-
-public interface FileMessengerDAO {
-	void insert(FileMessengerVO fileMessger);
-	public FileMessengerVO selectByMessenger(int messengerIdx);
-}
